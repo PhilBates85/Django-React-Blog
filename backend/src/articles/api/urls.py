efd04ea2686cea330caafbf14044d10607c4a127
PhilBates85 <phillipmbates85@gmail.com>
2020-05-31 11:15:13 -0500

    added Rest_framework View Set for CRUD OPS

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path 
+#from django.urls import path 
+#from .views import ArticleListView, ArticleDetailView
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import ArticleListView, ArticleDetailView
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, base_name='articles')
+urlpatterns = router.urls
 
-
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
-]
+#urlpatterns = [
+#    path('', ArticleListView.as_view()),
+#    path('<pk>', ArticleDetailView.as_view()),
+#]
